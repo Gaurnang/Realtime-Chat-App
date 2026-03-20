448846d337e1f0462c37fc5a6de0e9fc59793564
Gaurang <gauranggoel250@gmail.com>
2026-03-20 22:09:45 +0530

    Implemented Signup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,86 @@
+import Users from '../models/User.js';
+import bcrypt from 'bcryptjs';
+import { generateToken } from '../lib/utils.js';
+
+export const signup = async (req, res) => {
+    const {fullName, email, password} = req.body;
+
+    try {
+
+        if(!fullName || !email || !password) {
+            return res.status(400).json({ message: 'Full name, email and password are required!' });
+        }
+        if(password.length < 6) {
+            return res.status(400).json({ message: 'Password must be at least 6 characters long!' });
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(!emailRegex.test(email)) {
+            return res.status(400).json({ message: 'Invalid email format!' });
+        }
+        
+        const user = await Users.findOne({ email });
+        if(user) {
+            return res.status(400).json({ message: 'Email already exists!' });
+        }
+        
+       
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+
+        const newUser = new Users({
+            fullName,
+            email,
+            password: hashedPassword
+        })
+
+        
+        if(newUser) {
+            const token = generateToken(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id, 
+                email: newUser.email, 
+                fullName: newUser.fullName, 
+                profilePicture: newUser.profilePicture,
+                token: token
+            });
+        }
+
+        else {
+            return res.status(400).json({ message: 'Error creating user!' });
+        }
+
+    } catch (error) {
+        console.error('Signup error:', error);
+        res.status(500).json({ message: 'Server error!' });
+    }
+};
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    
+    if(!email || !password) {
+        return res.status(400).json({ message: 'Email and password are required!' });
+    }
+
+    if(password.length < 6) {
+        return res.status(400).json({ message: 'Password must be at least 6 characters long!' });
+    }
+    try {
+        const user = await Users.findOne({ email });
+        if(!user) {
+            return res.status(400).json({ message: 'Invalid email or password!' });
+        }
+        res.json({ message: `User ${email} logged in successfully!` });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error!' });
+    }   
+}
+
+export const logout = (req, res) => {
+    res.json({ message: 'Logout successful!' });
+}
+
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import {signup, login, logout } from '../controllers/auth.controller.js';  
+
+dotenv.config();
 
 const router = express.Router();
 
-router.get('/signup', (req, res) => {
-    res.send('Signup successful!');
-});
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/logout', logout);
 
-router.get('/login', (req, res) => {
-    res.send('Login successful!');
-});
-
-router.get('/logout', (req, res) => {
-    res.send('Logout successful!');
-});
 
 export default router;
